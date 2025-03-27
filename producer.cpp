916d7b3c77d214f14cc5e845192b87a4b2932143
Tristian Clayman <32968185+TrisClay345@users.noreply.github.com>
2025-03-27 20:36:01 +0000

    Doesn't work just saving this

--- producer.cpp
+++ producer.cpp
@@ -3,6 +3,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <iostream>
+#include <semaphore.h>
+#include <ctime>
+
 
 int main() {
     // Set up shared memory
@@ -11,14 +14,49 @@ int main() {
     int fd = shm_open("/shrdmemry", flags, mode);
     if(fd == -1) {
         std::cout << "An error occured with the shm_open function." << std::endl;
-        exit(-1);
+        exit(1);
     }
-    ftruncate(fd, 8);
+    // Resize shared memory
+    int err = ftruncate(fd, 128);
+    if(err = -1) {
+        std::cout << "An error occured with the ftruncate function" << std::endl;
+        exit(1);
+    }
 
-    // Start of critical section
+    // Get the memory in a read/writeable state
+    int prot = PROT_READ | PROT_WRITE;
+    void* tmp = mmap(NULL, 8, prot, MAP_SHARED, fd, 0);
+    if(tmp == MAP_FAILED) {
+        std::cout << "An error occured with the mmap function." << std::endl;
+        exit(1);
+    }
+    int* mem = static_cast<int*>(tmp);
 
-    // End of critical section
+    // Create shared semaphore
+    sem_t* semaphore;
+    semaphore = sem_open("/mysemaphore", flags, mode, 1); 
+    if(semaphore == SEM_FAILED) {
+        std::cout << "An error occured with the sem_open function." << std::endl;
+        exit(1);
+    }
 
-    
+    srand(time(nullptr));
+
+    int counter = 10'000;
+    mem[0] = -1;
+    mem[1] = -1;
+    while(true) {
+        sem_wait(semaphore);
+        // Start of critical section
+        if(mem[0] == -1) {
+            mem[0] = rand() % 100;
+        } else if(mem[1] == -1) {
+            mem[0] = rand() % 100;
+        }
+        // End of critical section
+        sem_post(semaphore);
+    }
+
+    sem_unlink("/mysemaphore");
     shm_unlink("/shrdmemry");
 }   
